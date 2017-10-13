d840e406ed1f36a0c8d8d95675b7877347ba16e3
ToluwaniO <ogunsanyatoluwani@gmail.com>
2017-10-13 15:04:36 -0400

    mdl

---
+++ app/js/index.js
@@ -0,0 +1,3 @@
+$('#myresume').on("click", function () {
+    window.open('https://drive.google.com/open?id=0B8YEC4zJxS3PWkc5ZTEzeFVXemc')
+})
--- js/index.js
+++
@@ -1,3 +0,0 @@
-$('#myresume').on("click", function () {
-    window.open('https://drive.google.com/open?id=0B8YEC4zJxS3PWkc5ZTEzeFVXemc')
-})
